4553df06c3004b6b65f7acfe2dade29536f5c9f8
brendomonteiro <brendomonteirodasilva@gmail.com>
2020-07-27 15:29:05 +0000

    Diminuindo o tempo de caregamento da pagina, isolando assets por controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( welcome.css
+                                                  coins.css 
+                                                  mining_types.css
+                                                  scaffolds.css
+                                                  welcome.css)
+
+Rails.application.config.assets.precompile += %w( application.js
+                                                  coins.js 
+                                                  mining_types.js
+                                                  welcome.js)
